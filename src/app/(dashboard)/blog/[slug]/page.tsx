cadd4a2de4b8acf46cace37b925d459f3ba72e0f
boning <boning.maple@gmail.com>
2026-01-07 17:43:20 +1100

    fix: no found mdx file should return 404

--- src/app/(dashboard)/blog/[slug]/page.tsx
+++ src/app/(dashboard)/blog/[slug]/page.tsx
@@ -1,14 +1,20 @@
+import { notFound } from "next/navigation";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
-  const { default: Post, _frontmatter } = await import(
-    `@/contents/${slug}.mdx`
-  );
-
-  return <Post />;
+  try {
+    const { default: Post, _frontmatter } = await import(
+      `@/features/design-system/pages/${slug}.mdx`
+    );
+    return <Post />;
+  } catch (error) {
+    console.error(error);
+    notFound();
+  }
 }
 
 export function generateStaticParams() {
---
+++ src/app/(dashboard)/not-found.tsx
@@ -0,0 +1,9 @@
+import NotFoundError from "@/features/errors/not-found-error";
+
+export default function NotFound() {
+  return (
+    <main className="flex-1 flex flex-col gap-6 p-4 w-full max-w-7xl mx-auto justify-center">
+      <NotFoundError />
+    </main>
+  );
+}
