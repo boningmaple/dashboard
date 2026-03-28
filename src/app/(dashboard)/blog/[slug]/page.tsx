5d6fddb7b2b07b8506896904404556f389b53a4b
boning <boning.maple@gmail.com>
2026-03-28 13:08:40 +1100

    refactor: rename contents directory to content

--- src/app/(dashboard)/blog/[slug]/page.tsx
+++ src/app/(dashboard)/blog/[slug]/page.tsx
@@ -8,7 +8,7 @@ export default async function Page({
 }) {
   const { slug } = await params;
   try {
-    const { default: Post } = await import(`@/contents/${slug}.mdx`);
+    const { default: Post } = await import(`@/content/${slug}.mdx`);
     return <Post />;
   } catch (error) {
     console.error(error);
@@ -17,7 +17,7 @@ export default async function Page({
 }
 
 export async function generateStaticParams() {
-  return await generatePostStaticParams("src/contents");
+  return await generatePostStaticParams("src/content");
 }
 
 export const dynamicParams = false;
